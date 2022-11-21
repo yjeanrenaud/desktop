d4905f021312daedb70567e5f01f81117016f3dd
Claudio Cambra <claudio.cambra@nextcloud.com>
2022-11-21 17:05:13 +0100

    Fix qtkeychain imports with Qt6
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/libsync/clientsideencryption.cpp
+++ src/libsync/clientsideencryption.cpp
@@ -34,6 +34,12 @@
 #include <QRandomGenerator>
 #include <QCryptographicHash>
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <qt6keychain/keychain.h>
+#else
+#include <qt5keychain/keychain.h>
+#endif
+
 #include <map>
 #include <string>
 #include <algorithm>
