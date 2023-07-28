f471d624bd7a41c3fd9a016ab61686af350f7c19
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2023-07-28 12:49:40 +0200

    add missing copyright notices
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/libsync/clientsideencryption.cpp
+++ src/libsync/clientsideencryption.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2017, Tomaz Canabrava <tcanabrava@kde.org>
+ * Copyright © 2020, Andreas Jellinghaus <andreas@ionisiert.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
 #define OPENSSL_SUPPRESS_DEPRECATED
 
 #include "clientsideencryption.h"
