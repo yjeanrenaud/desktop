frikke/desktop-1

Replace removed progress bar option orientation with state flag
Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

src/gui/folderstatusdelegate.cpp
@@ -321,7 +321,7 @@ void FolderStatusDelegate::paint(QPainter *painter, const QStyleOptionViewItem &
         pBarOpt.minimum = 0;
         pBarOpt.maximum = 100;
         pBarOpt.progress = overallPercent;
-        pBarOpt.orientation = Qt::Horizontal;
+        pBarOpt.state = QStyle::StateFlag::State_Horizontal;
         pBarOpt.rect = QStyle::visualRect(option.direction, option.rect, pBRect);
         QApplication::style()->drawControl(QStyle::CE_ProgressBar, &pBarOpt, painter, option.widget);
 

yjeanrenaud/desktop

src/gui/folderstatusdelegate.cpp
@@ -312,14 +312,13 @@ void FolderStatusDelegate::paint(QPainter *painter, const QStyleOptionViewItem &
         progressBarOpt.minimum = 0;
         progressBarOpt.maximum = 100;
         progressBarOpt.progress = overallPercent;
-        progressBarOpt.orientation = Qt::Horizontal;
+        progressBarOpt.state = QStyle::StateFlag::State_Horizontal;
         progressBarOpt.rect = QStyle::visualRect(option.direction, option.rect, progressBarRect);
 #ifdef Q_OS_MACOS
         backupStyle->drawControl(QStyle::CE_ProgressBar, &progressBarOpt, painter, option.widget);
 #else
         QApplication::style()->drawControl(QStyle::CE_ProgressBar, &progressBarOpt, painter, option.widget);
 #endif
-
 
         // Overall Progress Text
         QRect overallProgressRect;